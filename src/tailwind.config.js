908daaec88f146b6baa6377158620894a5243bcd
wendyhamel <whamel@it4web.nl>
2023-05-25 13:28:18 +0200

    Finished card styling

---
+++ src/tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Kumbh Sans', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				'cyan': 'hsl(185, 75%, 39%)',
+				'blue-dark': 'hsl(229, 23%, 23%)',
+				'blue-gray-dark': 'hsl(227, 10%, 46%)',
+				'gray': 'hsl(0, 0%, 59%)',
+			},
+		}
+	}
+}
